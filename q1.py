552f24aef15dd24bc175110de478fb277e03afb7
inflictt <legitsaksham@gmail.com>
2024-12-29 01:06:39 +0530

    adding requirements for q1.py

--- q1.py
+++ q1.py
@@ -1,4 +1,14 @@
 # Develop a number guessing game: The computer generates a random number, and the player has to guess it within a certain number of tries. Provide feedback to the player (e.g., "Too high!", "Too low!").
+
+# tasks to do
+# 1. make check logic a function
+# 2. write test cases for that function, eg: assert 3 == 3, assert 3 == 5 , assert 3 == somefunction(12)
+# 3. Handle string as value, handle blank as value, handle out of range values
+# 4. input range from user
+# 5. input attempts from user, where attempts should be less than equal to number of total values in the range
+# 6. Create a feedback loop via the logic function which returns high, low or correct
+
+
 import random
 num = random.randint(1,11)
 tries=5
