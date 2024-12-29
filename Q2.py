bb9de6d69796fe2982a1e84d4a51c2450bce21b6
inflictt <legitsaksham@gmail.com>
2024-12-30 01:30:15 +0530

    on enum

--- Q2.py
+++ Q2.py
@@ -1,13 +1,17 @@
 # Create a rock-paper-scissors game: Allow the user to play rock-paper-scissors against the computer. Use random number generation to determine the computer's choice.
-USER=input("Enter Your Name : ")
-DRAW="Draw"
-COMPUTER="Computer" 
+from enum import Enum
+
+class Name(Enum):
+    USER = input("enter your name:")          
+    COMPUTER = "Computer" 
+    DRAW = "Draw"  
+    
 def making_moves_input(round,round_winner_list):
     for index, i in enumerate(round,start = 1) :
 
         comp_move=computer_move()
         user_move= your_move()   
-        print(f"{index}) computer played : {comp_move} \t {index}) {USER} played :  {user_move}")
+        print(f"{index}) computer played : {comp_move} \t {index}) {Name.USER} played :  {user_move}")
         final_score= round_winner(comp_move,user_move)
         round_winner_list.append(final_score)
         print(f"Round {index} won by :  {round_winner_list[i]}")     
@@ -15,17 +19,17 @@ def making_moves_input(round,round_winner_list):
   
 def showcasing_round_winner(round,round_winner_list,user_score,comp_score):
     for index,i in enumerate(round,start=1) :    
-        if round_winner_list[i]==USER:
+        if round_winner_list[i]==Name.USER:
             user_score=user_score+1
-        elif round_winner_list[i]==COMPUTER:
+        elif round_winner_list[i]==Name.COMPUTER:
             comp_score=comp_score+1
     return user_score, comp_score
 
 
 def final_scoring_results(comp_score,user_score):
-    print(f"computer score is {comp_score} \t {USER} score is {user_score}")
+    print(f"computer score is {comp_score} \t {Name.USER} score is {user_score}")
     if user_score>comp_score:
-        print(f"{USER} won the game ")
+        print(f"{Name.USER} won the game ")
     elif user_score<comp_score:
         print("computer won the game ")
     else:
@@ -44,22 +48,22 @@ def checking_move_if_integer(user_message):
 def round_winner(comp_move,user_move):
 
     if comp_move==user_move:
-        return DRAW
+        return Name.DRAW
     if user_move==2:
         if comp_move==1:
-            return USER
+            return Name.USER
         else :
-            return COMPUTER 
+            return Name.COMPUTER 
     if user_move== 3 :
         if comp_move==2:
-            return USER
+            return Name.USER
         else :
-            return  COMPUTER 
+            return  Name.COMPUTER 
     if user_move==1:
         if comp_move==2:
-            return  COMPUTER 
+            return  Name.COMPUTER 
         else :
-            return USER
+            return Name.USER
     
     
 def computer_move():
@@ -68,7 +72,7 @@ def computer_move():
 
 def your_move():
     while True : 
-        move= checking_move_if_integer(f"Please {USER} enter: ")
+        move= checking_move_if_integer(f"Please {Name.USER} enter: ")
         if move >0 and move<4:
             return move 
         else:
@@ -89,12 +93,12 @@ final_scoring_results(comp_score,user_score)
 
 
     
-assert round_winner(1,1)==DRAW
-assert round_winner(2,2)==DRAW
-assert round_winner(3,3)==DRAW
-assert round_winner(1,2)==USER
-assert round_winner(1,3)== COMPUTER 
-assert round_winner(2,1)== COMPUTER 
-assert round_winner(2,3)==USER
-assert round_winner(3,1)==USER
-assert round_winner(3,2)== COMPUTER 
+assert round_winner(1,1)==Name.DRAW
+assert round_winner(2,2)==Name.DRAW
+assert round_winner(3,3)==Name.DRAW
+assert round_winner(1,2)==Name.USER
+assert round_winner(1,3)==Name.COMPUTER 
+assert round_winner(2,1)==Name.COMPUTER 
+assert round_winner(2,3)==Name.USER
+assert round_winner(3,1)==Name.USER
+assert round_winner(3,2)==Name.COMPUTER 
