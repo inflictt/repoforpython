e66d86bfb5ba07c53e6683453e756032dc3d7917
inflictt <legitsaksham@gmail.com>
2025-01-21 00:02:08 +0530

    final stages

--- to_do_list_2.py
+++ to_do_list_2.py
@@ -1,12 +1,18 @@
 from datetime import date
 from sympy import oo
 
+def creating_a_list(num_tasks,tasks_list):
+    for i in range(1, num_tasks + 1):
+        task_details = input(f"Enter the details for Task {i}: ")
+        tasks_list.append(task_details)
+
+                
 def number_of_tasks():
     while True:
         num_tasks=input("Enter the number of tasks you want to deal with : ")
         try:
             num_tasks=int(num_tasks)
-            if num_tasks>0 and num_tasks<oo:
+            if num_tasks>0:
                 return num_tasks
             else:
                 print("Wrong range inputed for number of tasks . ")
@@ -14,35 +20,55 @@ def number_of_tasks():
             print("Please enter a correct input . ")
         
         
-def checking_results_for_choice(choice,tasks_list,num_tasks):
+def checking_results_for_choice(choice,tasks_list,user_name):
         if choice==1: #means users want to create a list 
             print(f"Creating A list For {user_name}")
-            for i in range(1,num_tasks+1):
-                task_details=input(f"Enters the details for Task{i} = ")
-                i = i+1
-                tasks_list.append(task_details)
+            num_tasks=number_of_tasks()
+            creating_a_list(num_tasks,tasks_list)
+            
+        elif choice == 3:
+            print("deletion in progress please hold up a bit ...")
+            print("list before deleteing a value", tasks_list)
+            to_delete=input("enter the value you want to delete from the list ")
+            tasks_list.remove(to_delete)
+            print("list after deleting a value ",tasks_list)
         
+        elif choice==4 : #to view the list 
+            print("Viewving list here = ", tasks_list)
+                         
+            
+            
             
 def checking_user_input_for_integer():
-    while True :
-        choice = input("Enter 1 to create a list\nEnter 2 to edit/update the list\nEnter 3 to delete the list\nEnter 4 to view the list\nEnter 5 to exit the list\nPlease enter your choice here : ")
-        try :
-            choice= int(choice)
-            if (choice>0 and choice<6):
-                return choice 
+    while True:
+        choice = input(
+            "Enter 1 to create a list\n"
+            "Enter 2 to edit/update the list\n"
+            "Enter 3 to delete tasks\n"
+            "Enter 4 to view the list\n"
+            "Enter 5 to exit\n"
+            "Please enter your choice here: "
+        )
+        try:
+            choice = int(choice)
+            if 1 <= choice <= 5:
+                return choice
             else:
-                print("wrong range inputed . ")
+                print("Please enter a number between 1 and 5.")
         except:
-            print("Entered value is not a correct input for the choice . ")
+                print("Invalid input. Please enter a number.")
             
-            
-user_name=input("Enter Your Name  : ")
-print(f"Welcome {user_name} to the task manager application . ")
-choice = checking_user_input_for_integer()
-tasks_list=[]
-if choice==5:
-        print("Exitng the application....")
-else:            
-    num_tasks=number_of_tasks()
-    checking_results_for_choice(choice,tasks_list,num_tasks)
-print(tasks_list)
+def task_manager():
+    user_name = input("Enter Your Name: ")
+    print(f"Welcome {user_name} to the task manager application.")
+    tasks_list = []
+
+    while True:
+        choice = checking_user_input_for_integer()
+        if choice == 5:
+            print("Exiting the application...")
+            break
+        else:
+            checking_results_for_choice(choice, tasks_list, user_name)
+
+task_manager()
