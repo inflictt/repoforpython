a53955367d2755759bc0e11ce585ae51f33b832c
inflictt <legitsaksham@gmail.com>
2025-01-21 10:45:51 +0530

    task completed also added now

--- to_do_list_2.py
+++ to_do_list_2.py
@@ -20,17 +20,17 @@ def number_of_tasks():
             print("Please enter a correct input . ")
         
         
-def checking_results_for_choice(choice,tasks_list,user_name):
+def checking_results_for_choice(choice,tasks_list,user_name,completed_tasks_list):
         if choice==1: #means users want to create a list 
             print(f"Creating A list For {user_name}")
             num_tasks=number_of_tasks()
             creating_a_list(num_tasks,tasks_list)
         
         elif choice == 2 :#updation or edition depening on usereneed
-            print("editing/updating the list please hold up a bit ...")
+            print("relpacing the list please hold up a bit ...")
             to_edit=int(input("Enter the index of value you want to replace with : "))
             tasks_list[to_edit]=input("Enter the new value you want to update to : ")
-            print("List after getting updatd is : ", tasks_list)
+            print("List after getting replaced is : ", tasks_list)
         
         elif choice == 3: #delteing by value from the user need 
             print("deletion in progress please hold up a bit ...")
@@ -41,7 +41,12 @@ def checking_results_for_choice(choice,tasks_list,user_name):
         
         elif choice==4 : #to view the list 
             print("Viewving list here = ", tasks_list)
-                         
+        
+        elif choice == 5: #for completed task list
+            comp_task=int(input("enter the index of the task you want to update as completed : "))
+            x=tasks_list.pop(comp_task)
+            completed_tasks_list.append(x)          
+            print("Completed task list is ", completed_tasks_list)     
             
             
             
@@ -49,15 +54,16 @@ def checking_user_input_for_integer():
     while True:
         choice = input(
             "Enter 1 to create a list\n"
-            "Enter 2 to edit/update the list\n"
+            "Enter 2 to replace the list\n"
             "Enter 3 to delete tasks\n"
             "Enter 4 to view the list\n"
-            "Enter 5 to exit\n"
+            "Enter 5 to update task in completed task list\n  "
+            "Enter 6 to exit\n"
             "Please enter your choice here: "
         )
         try:
             choice = int(choice)
-            if 1 <= choice <= 5:
+            if 1 <= choice <= 6:
                 return choice
             else:
                 print("Please enter a number between 1 and 5.")
@@ -68,13 +74,14 @@ def task_manager():
     user_name = input("Enter Your Name: ")
     print(f"Welcome {user_name} to the task manager application.")
     tasks_list = []
+    completed_tasks_list=[]
 
     while True:
         choice = checking_user_input_for_integer()
-        if choice == 5:
+        if choice == 6:
             print("Exiting the application...")
             break
         else:
-            checking_results_for_choice(choice, tasks_list, user_name)
+            checking_results_for_choice(choice, tasks_list, user_name,completed_tasks_list)
 
 task_manager()
